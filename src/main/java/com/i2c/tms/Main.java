fc6bac75acdd0464a11a19519db7aa94e1593c04
AqibChattha <aqibali543@gmail.com>
2024-09-03 08:09:48 +0500

    message fixed.

--- src/main/java/com/i2c/tms/Main.java
+++ src/main/java/com/i2c/tms/Main.java
@@ -14,18 +14,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, JAXBException, ExecutionException {
-        String resource = Lib.readResource("admi998.xml");
+        String resource = Lib.readFile("C:\\Users\\AQIB\\Documents\\GitHub\\FedMessage\\src\\main\\resources\\admi998.xml");
 
-        JaxbContextLoader.INSTANCE.setCacheImpl(new JaxbContextCacheImpl());
-        JaxbContextLoader.INSTANCE.get(MxAdmi99800102.class, MxAdmi99800102._classes);
-        List<Class> classes = new ArrayList<>(Arrays.stream(MxAdmi99800102._classes).toList());
-        classes.addAll(Arrays.asList(MxInnerDocument.class, Admi998SuplDataV01.class, PtcptFile.class, PtcptPrfl.class));
-
-        // classes list to new array
-        Class[] classesArray = new Class[classes.size()];
-        classesArray = classes.toArray(classesArray);
-
-//        JaxbContextLoader.INSTANCE.get(SupplementaryDataEnvelope1.class, classesArray);
+//        JaxbContextLoader.INSTANCE.setCacheImpl(new JaxbContextCacheImpl());
+//        JaxbContextLoader.INSTANCE.get(MxAdmi99800102.class, MxAdmi99800102._classes);
+//        List<Class> classes = new ArrayList<>(Arrays.stream(MxAdmi99800102._classes).toList());
+//        classes.addAll(Arrays.asList(MxInnerDocument.class, Admi998SuplDataV01.class, PtcptFile.class, PtcptPrfl.class));
+//
+//        // classes list to new array
+//        Class[] classesArray = new Class[classes.size()];
+//        classesArray = classes.toArray(classesArray);
+//
+////        JaxbContextLoader.INSTANCE.get(SupplementaryDataEnvelope1.class, classesArray);
 
         MxAdmi99800102 mx = new MxAdmi99800102(resource);
 
@@ -33,11 +33,13 @@ public class Main {
 
         MxInnerDocument mxInnerDocument = new MxInnerDocument(any.get(0));
 
-        mx.getAdmstnPrtryMsg().getPrtryData().getData().setAny(mxInnerDocument);
 
-        MxWriteConfiguration mxWriteConfiguration = new MxWriteConfiguration(JAXBContext.newInstance(classesArray));
-        MxReadConfiguration mxReadConfiguration = new MxReadConfiguration();
 
-        System.out.println(mx.message(mxWriteConfiguration));
+        mx.getAdmstnPrtryMsg().getPrtryData().getData().setAny(mxInnerDocument.element(JAXBContext.newInstance(MxInnerDocument._classes)));
+
+//        MxWriteConfiguration mxWriteConfiguration = new MxWriteConfiguration(JAXBContext.newInstance(classesArray));
+//        MxReadConfiguration mxReadConfiguration = new MxReadConfiguration();
+
+        System.out.println(mx.message());
     }
 }
